ea1f21d845539f6ae60cd6c5c6f07a4581bb97ae
Rober MH <rmh.dev@gmail.com>
2019-04-19 12:24:48 +0800

    Test commit in store

--- tests/unit/store/config.spec.js
+++ tests/unit/store/config.spec.js
@@ -3,6 +3,7 @@ import { createLocalVue } from '@vue/test-utils'
 import Vuex from 'vuex'
 import config from '@/store/config'
 import cloneDeep from 'lodash.clonedeep'
+import {CustomerDataBuilder} from "../domain/model/customer/CustomerDataBuilder";
 
 describe('store config', () => {
     it('should have an empty repository of customers when created', () => {
@@ -11,7 +12,7 @@ describe('store config', () => {
         const store = new Vuex.Store(cloneDeep(config));
         expect(store.getters.getRepository.count()).eq(0);
     });
-    it('should be able to create and add a new customer', () => {
+    it('should be able to dispatch the creation and addition of a new customer', () => {
         const localVue = createLocalVue();
         localVue.use(Vuex);
         const localStore = new Vuex.Store(cloneDeep(config));
@@ -20,5 +21,15 @@ describe('store config', () => {
         localStore.dispatch('createAndAddNewCustomer', { 'name': 'Lorem Store' });
         expect(localStore.getters.getRepository.count()).eq(1);
         expect(localStore.getters.getRepository.all()[0].name.value).eq('Lorem Store');
+    });
+    it('should be able to commit a new customer', () => {
+        const localVue = createLocalVue();
+        localVue.use(Vuex);
+        const localStore = new Vuex.Store(cloneDeep(config));
+
+        expect(localStore.getters.getRepository.count(), 'Initial repository should be empty').eq(0);
+        localStore.commit('addCustomer', CustomerDataBuilder.aCustomer().withName('Lorem Commit').build());
+        expect(localStore.getters.getRepository.count()).eq(1);
+        expect(localStore.getters.getRepository.all()[0].name.value).eq('Lorem Commit');
     });
 });
