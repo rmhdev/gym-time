18eb5eea2dfcc2f6041da6d95e5903fd56dac2ec
Rober MH <rmh.dev@gmail.com>
2019-05-02 23:06:39 +0800

    The list of customers should be available in every moment

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,51 @@
 <template>
-  <div id="app" class="container">
+    <div id="app" class="container">
 
-    <div class="text-center">
-      <Welcome msg="Welcome!"/>
-      <Clock/>
-    </div>
+        <div class="text-center">
+            <Welcome msg="Welcome!"/>
+            <Clock/>
+        </div>
 
-    <div v-if="hasCheckoutCustomer">
-      <checkout
-        class="gym-customer-confirm-checkout"
-        :customer="getCheckoutCustomer"
-      ></checkout>
+        <div class="row justify-content-md-center">
+            <div class="col-5">
+                <checkout
+                    v-if="hasCheckoutCustomer"
+                    class="gym-customer-confirm-checkout"
+                ></checkout>
+                <checkin
+                    v-else
+                ></checkin>
+            </div>
+            <div class="col-1"></div>
+            <div class="col-6">
+                <customers></customers>
+            </div>
+        </div>
     </div>
-
-    <div v-else class="row justify-content-md-center">
-      <div class="col-5">
-        <checkin></checkin>
-      </div>
-      <div class="col-1"></div>
-      <div class="col-6">
-        <customers></customers>
-      </div>
-    </div>
-  </div>
 </template>
 
 <script>
-import Welcome from './components/Welcome.vue'
-import Clock from "./components/Clock";
-import Checkin from "./components/Checkin";
-import Checkout from "./components/Checkout";
-import Customers from "./components/Customers";
+    import Welcome from './components/Welcome.vue'
+    import Clock from "./components/Clock";
+    import Checkin from "./components/Checkin";
+    import Checkout from "./components/Checkout";
+    import Customers from "./components/Customers";
 
-export default {
-  name: 'app',
-  computed: {
-    hasCheckoutCustomer() {
-      return this.$store.getters.getCheckoutCustomers.length > 0;
-    },
-    getCheckoutCustomer() {
-      return this.$store.getters.getCheckoutCustomers;
+    export default {
+        name: 'app',
+        computed: {
+            hasCheckoutCustomer() {
+                return this.$store.getters.getCheckoutCustomers.length > 0;
+            }
+        },
+        components: {
+            Checkout,
+            Clock,
+            Welcome,
+            Checkin,
+            Customers
+        }
     }
-  },
-  components: {
-    Checkout,
-    Clock,
-    Welcome,
-    Checkin,
-    Customers
-  }
-}
 </script>
 
 <style lang="scss">
--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -1,11 +1,14 @@
 import {expect} from 'chai'
 import {createLocalVue, shallowMount} from '@vue/test-utils'
 import App from '@/App.vue'
+import Checkout from "@/components/Checkout.vue";
+import Customers from "@/components/Customers";
 import storeConfig from "@/store/config";
 import {CustomerDataBuilder} from "./domain/model/customer/CustomerDataBuilder";
-import Checkout from "@/components/Checkout.vue";
 import Vuex from "vuex";
 import cloneDeep from "lodash.clonedeep";
+import Checkin from "../../src/components/Checkin";
+
 
 const localVue = createLocalVue();
 localVue.use(Vuex);
@@ -27,6 +30,16 @@ describe('App.vue', () => {
         expect(
             wrapper.findAll(Checkout).length,
             'The Checkout component should be visible'
+        ).eq(1);
+
+        expect(
+            wrapper.findAll(Checkin).length,
+            'The Checkin component should not be visible'
+        ).eq(0);
+
+        expect(
+            wrapper.findAll(Customers).length,
+            'The Customers component should be visible'
         ).eq(1);
     });
 });
