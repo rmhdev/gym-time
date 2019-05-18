389c0d29dc7c54c57759a2e695cb9b26d089faea
Rober MH <rmh.dev@gmail.com>
2019-05-18 22:10:45 +0800

    Improve styles

--- src/components/Customers.vue
+++ src/components/Customers.vue
@@ -1,26 +1,39 @@
 <template>
     <div>
-        <search
-            :categories="categories"
-            :statuses="statuses"
-            v-on:search:category="searchCategory($event)"
-            v-on:search:status="searchStatus($event)"
-        ></search>
-        <div class="list-group gym-customers">
-            <div v-if="isEmpty" class="gym-empty alert alert-info text-center" role="alert">
-                <h4 class="gym-title alert-heading">Gym is empty!</h4>
-                <p>Looks like you'll be the first one!</p>
+        <div class="card">
+            <div class="card-header">
+                <search
+                    :categories="categories"
+                    :statuses="statuses"
+                    v-on:search:category="searchCategory($event)"
+                    v-on:search:status="searchStatus($event)"
+                ></search>
             </div>
 
-            <customer
-                v-else
-                class="gym-customer"
-                custom-class="list-group-item list-group-item-action"
-                v-for="customer in customers"
-                :key="customer.id.value"
-                :customer="customer"
-            ></customer>
+            <div class="gym-customers">
+                <div v-if="isEmpty" class="gym-empty alert alert-info text-center" role="alert">
+                    <h4 class="gym-title alert-heading">Gym is empty!</h4>
+                    <p>Looks like you'll be the first one!</p>
+                </div>
+
+                <template v-else>
+                    <div class="list-group list-group-flush">
+                        <customer
+                            class="gym-customer"
+                            custom-class="list-group-item list-group-item-action"
+                            v-for="customer in customers"
+                            :key="customer.id.value"
+                            :customer="customer"
+                        ></customer>
+                    </div>
+                    <div class="card-footer text-muted">
+                        test
+                    </div>
+                </template>
+            </div>
         </div>
+
+
     </div>
 </template>
 
