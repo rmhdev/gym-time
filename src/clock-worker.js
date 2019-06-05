a8dafaed5140a69c4e7368cd3e3561e7238a9733
Rober MH <rmh.dev@gmail.com>
2019-06-05 12:22:34 +0800

    Update datetime with a web worker

---
+++ src/clock-worker.js
@@ -0,0 +1,82 @@
+/**
+ * Web worker that sends the new datetime every given seconds
+ */
+
+(function () {
+
+    const UPDATE_EVERY_SECONDS = 60;
+
+    let timeout = null;
+    let interval = null;
+
+    /**
+     * Update datetime when seconds reach 0
+     *
+     * @param callback
+     */
+    let updateFirstTime = (callback) => {
+        let secondsToZero = 0;
+        let compareTo = UPDATE_EVERY_SECONDS >= 60 ? 0 : UPDATE_EVERY_SECONDS;
+        let now = new Date();
+        if (now.getSeconds() !== compareTo) {
+            secondsToZero = UPDATE_EVERY_SECONDS - (now.getSeconds() % (UPDATE_EVERY_SECONDS));
+        }
+        stop();
+        timeout = setTimeout(function() {
+            sendNewDatetime();
+            if (callback && typeof(callback) === 'function') {
+                callback();
+            }
+        }, Math.max(secondsToZero, 1) * 1000)
+    };
+
+    /**
+     * Update datetime every X seconds
+     */
+    let updateManyTimes = () => {
+        stop();
+        interval = setInterval(() => {
+            sendNewDatetime();
+        }, UPDATE_EVERY_SECONDS * 1000);
+    };
+
+    /**
+     * Send message with new datetime
+     */
+    let sendNewDatetime = () => {
+        postMessage((new Date()).toISOString());
+    };
+
+    let start = () => {
+        stop();
+        updateFirstTime(updateManyTimes);
+    };
+
+    let stop = () => {
+        if (timeout) {
+            clearTimeout(timeout);
+        }
+        if (interval) {
+            clearInterval(interval);
+        }
+    };
+
+    onmessage = function(event) {
+        if (event.data === undefined ) {
+            return;
+        }
+        if (event.data[0] === undefined) {
+            return;
+        }
+        if (event.data[0] !== 'action') {
+            return;
+        }
+        if (event.data[1] === undefined) {
+            return;
+        }
+        if ('start' === event.data[1]) {
+            start();
+        }
+        stop();
+    };
+})();
--- src/main.js
+++ src/main.js
@@ -3,10 +3,27 @@ import App from './App.vue'
 import store from './store/index'
 import './registerServiceWorker'
 import './styles/main.scss'
+import ClockWorker from 'worker-loader!./clock-worker.js';
 
 Vue.config.productionTip = false;
 
 new Vue({
-  store,
-  render: h => h(App)
+    store,
+    render: h => h(App)
 }).$mount('#app');
+
+if (['production', 'development'].includes(process.env.NODE_ENV || '')) {
+    // worker
+    (function (store) {
+        let w = null;
+        if (typeof (Worker) !== 'undefined') {
+            if (w === null) {
+                w = new ClockWorker();
+                w.postMessage(['action', 'start']);
+            }
+            w.onmessage = function (event) {
+                store.dispatch('updateDatetime', event.data);
+            };
+        }
+    })(store);
+}
